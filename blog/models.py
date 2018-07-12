2059596799898a5e88d876bd56caea59be8e71d4
Waqar Ahmad Khan <waqarbinmuhammad@hotmail.com>
2018-07-12 07:22:14 +0500

    Jobs and blogis ready

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+# Create A Blog models
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+
+#Add Blogs app to settings
+
+
+
+
+
+
+
+
+
+
+#create migrations , migrate , add to admin
